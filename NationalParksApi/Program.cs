5b4f0066b1ed40e17ba8cdcfd69cae41e9d48cde
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-10 10:51:57 -0700

    add config for EF core

--- NationalParksApi/Program.cs
+++ NationalParksApi/Program.cs
@@ -1,9 +1,19 @@
+using NationalParksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<NationalParksApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
