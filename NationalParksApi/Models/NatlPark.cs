d6926d7146469320a6577784202762dcc15f9bcc
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-10 13:07:37 -0700

    add stateId foreign key

--- NationalParksApi/Models/NatlPark.cs
+++ NationalParksApi/Models/NatlPark.cs
@@ -5,4 +5,5 @@ public class NatlPark
     public int NatlParkId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public int StateId { get; set; }
 }
