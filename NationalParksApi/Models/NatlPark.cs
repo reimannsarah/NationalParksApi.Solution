b110498beae5821a5a6a3945836c7444fb1ecca6
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-10 14:05:22 -0700

    remove ref nav and change foreign key annotation

--- NationalParksApi/Models/NatlPark.cs
+++ NationalParksApi/Models/NatlPark.cs
@@ -7,7 +7,7 @@ public class NatlPark
     public int NatlParkId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public int StateId { get; set; }
     [ForeignKey("StateId")]
-    public State State { get; set; }
+    public int StateId { get; set; }
+    // public State State { get; set; }
 }
