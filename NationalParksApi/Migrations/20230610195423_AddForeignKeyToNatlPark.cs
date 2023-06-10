c8bcddf344f51c4d0875d8cb8c63011e6e8faf5b
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-10 13:06:32 -0700

    add stateid foreign key to natlpark

---
+++ NationalParksApi/Migrations/20230610195423_AddForeignKeyToNatlPark.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NationalParksApi.Migrations
+{
+    public partial class AddForeignKeyToNatlPark : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StateId",
+                table: "NatlParks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                table: "NatlParks");
+        }
+    }
+}
