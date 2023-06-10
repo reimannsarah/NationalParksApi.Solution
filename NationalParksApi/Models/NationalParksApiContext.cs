378057d34d22167dc0339beab95ad3156cfe92c5
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-10 11:54:06 -0700

    fix typos

--- NationalParksApi/Models/NationalParksApiContext.cs
+++ NationalParksApi/Models/NationalParksApiContext.cs
@@ -12,11 +12,11 @@ public class NationalParksApiContext : DbContext
   {
     builder.Entity<NatlPark>()
       .HasData(
-        new NatlPark { NatlParkId = 1, Name = "Acadia", Description = "Features the tallest mountain on the Atlantic Coast of the United States.", StateId = 19},
-        new NatlPark { NatlParkId = 2, Name = "Arches", Description = "Features more than 2,000 natural sanstone arches.", StateId = 44},
-        new NatlPark { NatlParkId = 3, Name = "Badlands", Description = "Collection of buttes, pinnacles, spires, and mixed grass prairies.", StateId = 41},
-        new NatlPark { NatlParkId = 4, Name = "Big Bend", Description = "Named for the prominent bend in the Rio Grande along the U.S. -- Mexico border.", StateId = 43},
-        new NatlPark { NatlParkId = 5, Name = "Biscayne", Description = "Mostly underwater park at the north end of the Florida Keys.", StateId = 19}
-      )
+        new NatlPark { NatlParkId = 1, Name = "Acadia", Description = "Features the tallest mountain on the Atlantic Coast of the United States."},
+        new NatlPark { NatlParkId = 2, Name = "Arches", Description = "Features more than 2,000 natural sanstone arches."},
+        new NatlPark { NatlParkId = 3, Name = "Badlands", Description = "Collection of buttes, pinnacles, spires, and mixed grass prairies."},
+        new NatlPark { NatlParkId = 4, Name = "Big Bend", Description = "Named for the prominent bend in the Rio Grande along the U.S. -- Mexico border."},
+        new NatlPark { NatlParkId = 5, Name = "Biscayne", Description = "Mostly underwater park at the north end of the Florida Keys."}
+      );
   }
 }
