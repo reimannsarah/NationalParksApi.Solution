4689d1ece09673b04db8a50a56cc0e25d8992f14
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-10 13:33:14 -0700

    add list on parks to state model

---
+++ NationalParksApi/Migrations/20230610200846_AddListParksToStateEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NationalParksApi.Migrations
+{
+    public partial class AddListParksToStateEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_NatlParks_StateId",
+                table: "NatlParks",
+                column: "StateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NatlParks_States_StateId",
+                table: "NatlParks",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "StateId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NatlParks_States_StateId",
+                table: "NatlParks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NatlParks_StateId",
+                table: "NatlParks");
+        }
+    }
+}
