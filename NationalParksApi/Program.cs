b28416180d330eb37a4d4b689da7858b14e144d3
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-10 11:53:51 -0700

    add config for versioning

--- NationalParksApi/Program.cs
+++ NationalParksApi/Program.cs
@@ -1,5 +1,10 @@
 using NationalParksApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +22,43 @@ builder.Services.AddDbContext<NationalParksApiContext>(
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                                        opt.ReportApiVersions = true;
+                                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                                    });
+
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
 var app = builder.Build();
+
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => 
+    {
+      foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+          {
+              options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                  description.GroupName.ToUpperInvariant());
+          }
+    });
 }
 
 else 
@@ -31,10 +66,70 @@ else
   app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthorization();
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
 app.Run();
+
+public class ConfigureSwaggerOptions
+    : IConfigureNamedOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+
+    public ConfigureSwaggerOptions(
+        IApiVersionDescriptionProvider provider)
+    {
+        _provider = provider;
+    }
+
+    /// <summary>
+    /// Configure each API discovered for Swagger Documentation
+    /// </summary>
+    /// <param name="options"></param>
+    public void Configure(SwaggerGenOptions options)
+    {
+        // add swagger document for every API version discovered
+        foreach (var description in _provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(
+                description.GroupName,
+                CreateVersionInfo(description));
+        }
+    }
+
+    /// <summary>
+    /// Configure Swagger Options. Inherited from the Interface
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="options"></param>
+    public void Configure(string name, SwaggerGenOptions options)
+    {
+        Configure(options);
+    }
+
+    /// <summary>
+    /// Create information about the version of the API
+    /// </summary>
+    /// <param name="description"></param>
+    /// <returns>Information about the API</returns>
+    private OpenApiInfo CreateVersionInfo(
+            ApiVersionDescription desc)
+    {
+        var info = new OpenApiInfo()
+        {
+            Title = ".NET Core (.NET 6) Web API",
+            Version = desc.ApiVersion.ToString()
+        };
+
+        if (desc.IsDeprecated)
+        {
+            info.Description += " This API version has been deprecated. Please use one of the new APIs available from the explorer.";
+        }
+
+        return info;
+    }
+}
+
